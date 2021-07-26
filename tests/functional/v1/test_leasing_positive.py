828cd97965e68707bbfbe682621d3aa4a728720a
stack <samuel_zuk@student.uml.edu>
2021-07-26 15:45:34 +0000

    WIP: First round of positive leasing tests

--- tests/functional/v1/test_leasing_positive.py
+++ tests/functional/v1/test_leasing_positive.py
@@ -11,28 +11,39 @@ class PositiveLeasingTests(base.EsiFunctionalBase):
         cls._init_client(cls, 'lessee')
 
         cls.dummy_node = cls._new_dummy_node(cls, 'owner')
-        cls.offers = {}
-        cls.leases = {}
 
     def setUp(self):
         super(PositiveLeasingTests, self).setUp()
         self.clients = PositiveLeasingTests.clients
-        self.dummy_node = PositiveLeasingTests.dummy_node
 
-    def test_offer_create(self):
-        """ Tests basic functionality for offer create.
-            Checks if offer details were returned.
-            Will update self.offers with the created offer. """
+    def test_offer_self(self):
+        """ Tests basic functionality of "esi offer create/list/show/delete"
+                when executed by a node-owning project.
+            Test steps:
+            1) Create an offer for an owned node.
+            2) Check that offer details were returned.
+            3) Check that the output of 'offer list' contains the new offer.
+            4) Check that calling 'offer show' with the offer's uuid returns
+                the details of the offer.
+            5) Delete the offer created in step 1.
+            6) Check that the output of 'offer list' does not contain the
+                deleted offer. """
         offer = self.offer_create('owner', self.dummy_node)
         self.assertNotEqual(offer, {})
 
-        self.offers.update({ 'test_offer': offer })
-
-    def test_offer_list(self):
-        """ Tests that node owner can list offers they created.
-            Checks that a listing was returned and that the uuid from the
-                offer previously created is present in the list. """
         listings = self.offer_list('owner')
         self.assertNotEqual(listings, [])
-        self.assertIn(self.offers['test_offer']['uuid'],
-                [x['UUID'] for x in listings])
+        self.assertIn(offer['uuid'], [x['UUID'] for x in listings])
+
+        details = self.offer_show('owner', offer['uuid'])
+        for field in offer.keys():
+            self.assertEqual(offer[field], details[field])
+        
+        self.offer_delete('owner', offer['uuid'])
+        listings = self.offer_list('owner')
+        self.assertNotIn(offer['uuid'], [x['UUID'] for x in listings])
+
+    def test_offer_lessee(self):
+        """ Tests basic functionality of "esi offer claim/list" and "esi lease
+                list/show/delete" when used by a lessee."""
+        self.assertEqual(0, 0)
